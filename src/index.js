fbb2067d8d0ced5c92a6890262c3b2b30310134c
mitan11 <mitantank00@gmail.com>
2025-06-12 10:43:50 +0530

    `Refactor package.json and src/index.js to remove unnecessary imports and dependencies`

--- src/index.js
+++ src/index.js
@@ -1,39 +1,42 @@
-import express from 'express'
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import authRoutes from './routes/authRoute.js'
 import messageRoutes from './routes/messageRoute.js'
-import dotenv from 'dotenv'
-import { connectDB } from './lib/db.js'
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
-import { app , server } from './lib/socket.js'
+import path from "path";
 
-dotenv.config()
+import { connectDB } from "./lib/db.js";
 
-// port
-const PORT = process.env.PORT || 8080;
 
-// middleware
-app.use(express.json({ limit: '50mb' }))
-app.use(express.urlencoded({ limit: '50mb', extended: true }))
-app.use(cookieParser())
-app.use(cors({
-    origin: ['http://localhost:5173', 'https://chat-app-khaki-nu-55.vercel.app'],
-    credentials: true
-}))
-// 
-app.get("/", (req, res) => {
-    res.send("Hello World , Server is working")
-})
+import { app, server } from "./lib/socket.js";
 
-// 
-app.use("/api/auth", authRoutes)
-app.use("/api/messages", messageRoutes)
+dotenv.config();
 
-server.listen(PORT, (error) => {
-    if (!error) {
-        console.log(`Server is running on port http://localhost:${PORT}`)
-        connectDB();
-        return
-    }
-    console.log(error)
-})
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
--- src/lib/socket.js
+++ src/lib/socket.js
@@ -8,8 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: ['http://localhost:5173', 'https://chat-app-khaki-nu-55.vercel.app'],
-        credentials: true
+        origin: ["http://localhost:5173"],
     }
 })
 
