bd4481edabb395458fddd0e07de0879af9f80689
Mitan tank <141761215+Mitan11@users.noreply.github.com>
2025-12-25 21:06:02 +0530

    Update CORS origin to allow requests from production URL

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://chat-app-pi-one-39.vercel.app",
     credentials: true,
   })
 );
